7d5b5f46af4c4b604283669fc64a2bd86e267549
Brandan Glendenning <notbrandan@gmail.com>
2015-09-08 16:09:02 -0400

    initial landing page views, urls, & templates

--- geokit/urls.py
+++ geokit/urls.py
@@ -2,8 +2,10 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 from builder import urls as builder_urls
+from account import urls as account_urls
 
 urlpatterns = [
+    url(r'^$', include(account_urls)),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^builder/', include(builder_urls)),
 ]
