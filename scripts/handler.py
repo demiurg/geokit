908f6d11803bd9c4a0e71a0a23a27de29a953223
bhbraswell <rbraswell@appliedgeosolutions.com>
2015-09-09 14:21:08 -0400

    handler makes data cubes

--- scripts/handler.py
+++ scripts/handler.py
@@ -2,10 +2,31 @@
 
 import datetime
 from collections import defaultdict
+import numpy as np
 from gipsproject import GIPSproject
 import config
 
 from pdb import set_trace
+
+
+class Datacube(object):
+
+    def __init__(self, variables, locationnames, dates):
+        self.variables = variables
+        self.locationnames = locationnames
+        self.dates = dates
+        self.nvariables = len(variables)
+        self.nlocations = len(locationnames)
+        self.ntimes = len(dates)
+        self.data = np.zeros((self.nvariables, self.nlocations, self.ntimes))
+        self.mask = np.zeros_like(self.data, dtype=int)
+
+    def insert(self, variable, locationname, date, value):
+        ivar = self.variables.index(variable)
+        iloc = self.locationnames.index(locationname)
+        idate = self.dates.index(date)
+        self.data[ivar, iloc, idate] = value
+        self.mask[ivar, iloc, idate] = 1
 
 
 def run_gips_project(variables, locations, times):
@@ -13,14 +34,14 @@ def run_gips_project(variables, locations, times):
     print "variables", variables
     print "locations", locations
     print "times", times
-    # collect by unique asset name and loop over assets
-    # because asset determines the command that will be used
+
     daterange = times[0]
+
     dates = str(daterange)
     key = str(config.locations[locations[0]]['key'])
     site = str(config.locations[locations[0]]['path'])
-
     where = str(locations[1])
+
     commandparams = defaultdict(dict)
     productinfo = defaultdict(list)
 
@@ -55,102 +76,111 @@ def run_gips_project(variables, locations, times):
     return inventories
    
 
-def aggregate(inventories):
-
-    # Each inventory is associated with a single GIPS project tree
+def aggregate(inventories, datacube):
+    # Each inventory is associated with a single location and GIPS project tree
     # the project tree has one or more products. Each product provides data 
-    # for one or more variables
-
+    # for one or more variables.
 
     for asset, item in inventories.items():
-
         inventories, params, info = item
-    
-        # print asset, inventories, params, info
-
-        # img = inv.data[inv.dates[0]].open(asset, None, False)
-
-        products = params['products']
-
         for varset in info:
-
             variable, product, layer = varset
-
             for inventory in inventories:
-                # each inventory is associated with a location
-
-                location = inventory.get_location()
-
+                try:
+                    location = inventory.get_location()
+                except Exception:
+                    set_trace()
                 dates = inventory.dates
-
                 for date in dates:
-
-                    # print date
-
                     img = inventory.data[date].open(product, None, False)
-
                     if layer is not None:
                         band = img[str(layer)]
                     else:
                         band = img[0]
-
                     missingval = band.NoDataValue()
                     data = band.Read()
-
                     try:
                         mean = data[data != missingval].mean()
-                    except:
-                        mean = -9999.
-                    print variable, location, date, mean
+                    except Exception:
+                        pass
+                    else:
+                        print variable, location, date, mean
+                        datacube.insert(variable, location, date, mean)
 
-                    # set_trace()
+    return datacube
+
+
+def handle(variables, locations, times):
+
+    # use GIPS to create image output
+    inventories = run_gips_project(variables, locations, times)
+
+    # times convert to dates
+    daterange = times[0].split(',')
+    startdate = datetime.datetime.strptime(daterange[0], '%Y-%j').date()
+    try:
+        enddate = datetime.datetime.strptime(daterange[1], '%Y-%j').date()
+    except:
+        enddate = startdate
+    ndates = (enddate - startdate).days
+    dates = [startdate + datetime.timedelta(i) for i in range(ndates+1)]
+
+    nvariables = len(variables)
+
+    # for the first inventory key, how many GIPS inventories are there
+
+    gipsinventories = inventories.values()[0][0]
+    locationnames = [i.get_location() for i in gipsinventories]
+    nlocations = len(locationnames)
+    # nlocations = len(inventories.values()[0][0])
+
+    datacube = Datacube(variables, locationnames, dates)
+
+    # collect results into datacube
+    if len(inventories) > 0:
+        datacube = aggregate(inventories, datacube)
+    else:
+        return None
+
+    set_trace()
+
+
+
 
 
 def example1():
+
     # location set
     source = "GADM2"
     query = "HASC_1='US.NH'"
     locations = (source, query)
-    # variable set
-    # variables = "ndvi_large, satvi_large, tsurf_large, tmax_conus, tmin_conus"
-    # variables = "ndvi_large, satvi_large, prcp_global"
-    # variables = "ndvi_large"
-    variables = "tave_global"
 
-    variables = variables.split(',')
-    variables = [v.strip() for v in variables]
+    # variable set
+    variables = "tmax_conus, ndvi_large"
+    variables = [v.strip() for v in variables.split(',')]
+
     # time set
-    daterange = "2015-193,2015-194"
+    daterange = "2014-193,2014-194"
     interval = "1-day"
     times = (daterange, interval)
-    # create images
+
+    result = handle(variables, locations, times)
+    print result
 
 
-    inventories = run_gips_project(variables, locations, times)
-
-
-    if len(inventories) > 0:
-        aggregate(inventories)
-    else:
-        print "there are no inventories"
-
-
-
-def example2():
-    # location set
-    source = "LargeLakesUS"
-    query = "uuid=4b14eba8-b36b-4170-81b6-6924c3f4b9d3"
-    locations = (source, query)
-    # variable set
-    variables = "tmin_conus,tmax_conus,lswi_large"
-    variables = variables.split(',')
-    variables = [v.strip() for v in variables]
-    # time set
-    daterange = "2015-201"
-    interval = "1-day"
-    times = (daterange, interval)
-    # create images
-    inventories = run_gips_project(variables, locations, times)
+# def example2():
+#     # location set
+#     source = "LargeLakesUS"
+#     query = "uuid=4b14eba8-b36b-4170-81b6-6924c3f4b9d3"
+#     locations = (source, query)
+#     # variable set
+#     variables = "tmin_conus,tmax_conus,lswi_large"
+#     # time set
+#     daterange = "2015-201"
+#     interval = "1-day"
+#     times = (daterange, interval)
+#     # create images
+#     inventories = run_gips_project(variables, locations, times)
 
 
 if __name__ == "__main__":
