09532222c880eb67467a1d1fe143eb59fa40acd9
bhbraswell <rbraswell@appliedgeosolutions.com>
2015-09-11 16:38:40 -0400

    handler scripts working ok, slowly

--- scripts/handler.py
+++ scripts/handler.py
@@ -20,30 +20,41 @@ class Datacube(object):
     def __init__(self, variables, locationnames, dates):
         # a variable has units and a physical definition
         # a location name is a unique reference to a spatial feature
-        self.variables = variables
-        self.locationnames = locationnames
-        self.dates = dates
+        self.variables = sorted(variables)
+        self.locationnames = sorted(locationnames)
+        self.dates = sorted(dates)
         self.nvariables = len(variables)
         self.nlocations = len(locationnames)
         self.ntimes = len(dates)
-        self.dims = (len(self.variables), len(self.locationnames), len(dates))
-        self.data = np.zeros((self.nvariables, self.nlocations, self.ntimes))
-        self.mask = np.zeros_like(self.data, dtype=int)
+        self.dims = (self.nvariables, self.nlocations, self.ntimes)
+        self._data = np.zeros(self.dims)
+        self._mask = np.zeros_like(self._data, dtype=int)
 
-    def insert(self, variable, locationname, date, value):
+    def set(self, variable, locationname, date, value):
         ivar = self.variables.index(variable)
         iloc = self.locationnames.index(locationname)
         idate = self.dates.index(date)
-        self.data[ivar, iloc, idate] = value
-        self.mask[ivar, iloc, idate] = 1
+        self._data[ivar, iloc, idate] = value
+        self._mask[ivar, iloc, idate] = 1
 
-    def load(self, data):
-        if data.shape != self.dims:
-            raise Exception('Data dimensions must be %s' % str(self.dims))
-        self.data = data
+    def get(self, variable, locationname, date):
+        ivar = self.variables.index(variable)
+        iloc = self.locationnames.index(locationname)
+        idate = self.dates.index(date)
+        if self._mask[ivar, iloc, idate] == 1:
+            return self._data[ivar, iloc, idate]
+        else:
+            return None
 
     def dump(self, filepath):
-        self.data.tofile(filepath, sep=',')
+        self._data.dump(filepath)
+
+    def load(self, filepath):
+        self._data = np.load(filepath)
+
+    def __getitem__(self, key):
+        ivar = self.variables.index(key)
+        return self._data[ivar,:,:].squeeze()
 
 
 class Datarequest(object):
@@ -54,6 +65,7 @@ class Datarequest(object):
         self.times = times
         self.inventories = None
         self.datacube = None
+        self.projid = None
 
     def run_gips_project(self):
         dates = str(self.times[0])
@@ -74,18 +86,18 @@ class Datarequest(object):
             commandparams[asset]['res'] = res
             try:
                 commandparams[asset]['products'].add(product)
-            except:
+            except Exception:
                 commandparams[asset]['products'] = set([product])
         self.inventories = dict()
         for asset in commandparams.keys():
             products = list(commandparams[asset]['products'])
             res = commandparams[asset]['res']
-            print asset, products, dates, key, site, where, res
             project = GIPSproject()
             project.set(asset, products, dates, key, site, where, res)
             inventory = project.run()
             if len(inventory) > 0:
                 self.inventories[asset] = (inventory, commandparams[asset], productinfo[asset])
+                self.projid = project.pid
 
     def aggregate_results(self):
         """ Each inventory is associated with a single location and GIPS project tree
@@ -100,7 +112,7 @@ class Datarequest(object):
                 locationnames.add(inventory.get_location())
         dates = list(dates)
         locationnames = list(locationnames)
-        self.datacube = Datacube(self.variables, locationnames, dates)
+        self.inv_datacube = Datacube(self.variables, locationnames, dates)
         for asset, item in self.inventories.items():
             inventories, params, info = item
             for varset in info:
@@ -124,8 +136,7 @@ class Datarequest(object):
                         except Exception:
                             pass
                         else:
-                            print variable, location, date, mean
-                            self.datacube.insert(variable, location, date, mean)
+                            self.inv_datacube.set(variable, location, date, mean)
 
     def initialize_data(self):
         # convert times to output dates
@@ -137,28 +148,24 @@ class Datarequest(object):
         except Exception:
             enddate = startdate
         lookup = {'day':rrule.DAILY, 'week':rrule.WEEKLY, 'month':rrule.MONTHLY}
-        self.out_dates = list(rrule.rrule(lookup[step], dtstart=startdate, until=enddate, interval=int(interval)))
-        self.num_out_dates = len(self.out_dates)
-        # get locationnames from the source
-
-        # locationpath = str(config.locations[self.locations[0]]['path'])
+        dates = list(rrule.rrule(lookup[step], dtstart=startdate, until=enddate, interval=int(interval)))
+        dates = [d.date() for d in dates]
+        # get locationnames from the sql-filtered source
+        # this will need to change when the data are in a database
         locationpath = self.filtered_locations()
-
         records = fiona.open(locationpath)
         key = str(config.locations[self.locations[0]]['key'])
-
-        self.out_locationnames = []
+        locationnames = []
         for record in records:
-            self.out_locationnames.append(record['properties'][key])
-        if len(self.out_locationnames) != len(set(self.out_locationnames)):
+            locationnames.append(record['properties'][key])
+        if len(locationnames) != len(set(locationnames)):
             raise Exception('location IDs are not unique')
-        else:
-            self.num_out_locationnames = len(self.out_locationnames)
         # create an empty datacube of the right dimensions 
-        self.out_datacube = Datacube(self.variables, self.out_locationnames, self.out_dates)
-
+        self.datacube = Datacube(self.variables, locationnames, dates)
 
     def filtered_locations(self):
+        """ Create a temporary shapefile that has the filtered locations
+        this won't be needed when the data are stored in a database """
         locationpath = str(config.locations[self.locations[0]]['path'])
         dirname, locationfile = os.path.split(locationpath)
         outpath = os.path.join(dirname, "TMP.shp")
@@ -171,84 +178,94 @@ class Datarequest(object):
         result = commands.getstatusoutput(cmd)
         return outpath
 
-
     def reduce_results(self):
-
-        set_trace()
-
+        """ fit the GIPS results into the output grid """
+        dates = self.datacube.dates
+        for idxvar, variable in enumerate(self.variables):
+            for idxloc, locationname in enumerate(self.datacube.locationnames):
+                data = np.zeros(len(dates))
+                count = np.zeros_like(data)
+                for jdxdate, inv_date in enumerate(self.inv_datacube.dates):
+                    found = False
+                    for idxdate, date in enumerate(dates):
+                        if idxdate < len(dates) - 1:
+                            if inv_date >= dates[idxdate] and inv_date < dates[idxdate+1]:
+                                found = True
+                        else:
+                            if inv_date >= dates[idxdate]:
+                                found = True
+                        if found is True:
+                            value = self.inv_datacube.get(variable, locationname, inv_date)
+                            if value is not None:
+                                data[idxdate] += value
+                                count[idxdate] += 1
+                            break
+                # there still might be missing values in the output grid so fill them
+                wvalid = np.where(count != 0)[0]
+                for idxdate, date in enumerate(dates):
+                    if count[idxdate] > 0:
+                        value = data[idxdate]/count[idxdate]
+                    else:
+                        value = np.interp(idxdate, wvalid, data[wvalid])  
+                    self.datacube.set(variable, locationname, date, value)
 
     def handle(self):
-
         # use GIPS to create image outputs; create self.inventories
         self.run_gips_project()
-        # determine the output dimensions of the data cube; create self.out_datacube
+        # determine the output dimensions of the data cube; create empty self.datacube
         self.initialize_data()
-        # collect results by variable, location and date; create self.datecube
+        # collect results by variable, location and date; create and fill self.inv_datecube
         self.aggregate_results()
-        # reframe aggregated GIPS results into the output grid
+        # reframe aggregated GIPS results into the output grid self.datacube
         self.reduce_results()
 
 
-
 def example1():
+
+    # variable set
+    variables = ("ndvi_large", "tmin_conus")
 
     # location set
     source = "GADM2"
     query = "HASC_1='US.NH'"
     locations = (source, query)
 
+    # time set
+    daterange = "2014-180,2014-200"
+    interval = "1-week"
+    times = (daterange, interval)
+
+    # create and fill the request
+    datarequest = Datarequest(variables, locations, times)
+    datarequest.handle()
+    datarequest.datacube.dump('handler_test1.pkl')
+    print datarequest.datacube['ndvi_large']
+    print datarequest.datacube['tmin_conus']
+
+
+def example2():
+
     # variable set
-    variables = "ndvi_large"
-    # variables = "ndvi_large, tmin_conus"
-    variables = tuple([v.strip() for v in variables.split(',')])
+    variables = ("tave_global", "lswi_large")
+
+    # location set
+    source = "LargeLakesUS"
+    query = "uuid=4b14eba8-b36b-4170-81b6-6924c3f4b9d3"
+    locations = (source, query)
 
     # time set
-    daterange = "2014-191,2014-199"
+    daterange = "2014-180,2014-200"
     interval = "1-day"
     times = (daterange, interval)
 
+    # create and fill the request
     datarequest = Datarequest(variables, locations, times)
     datarequest.handle()
-    datarequest.datacube.dump('handler_test.csv')
-
-
-
-
-# def example1():
-#     # location set
-#     source = "GADM2"
-#     query = "HASC_1='US.NH'"
-#     locations = (source, query)
-
-#     # variable set
-#     variables = "tmax_conus, ndvi_large"
-#     variables = tuple([v.strip() for v in variables.split(',')])
-
-#     # time set
-#     daterange = "2014-193,2014-194"
-#     interval = "1-day"
-#     times = (daterange, interval)
-
-#     result = handle(variables, locations, times)
-#     print result
-
-
-# def example2():
-#     # location set
-#     source = "LargeLakesUS"
-#     query = "uuid=4b14eba8-b36b-4170-81b6-6924c3f4b9d3"
-#     locations = (source, query)
-#     # variable set
-#     variables = "tmin_conus,tmax_conus,lswi_large"
-#     # time set
-#     daterange = "2015-201"
-#     interval = "1-day"
-#     times = (daterange, interval)
-#     # create images
-#     inventories = run_gips_project(variables, locations, times)
+    datarequest.datacube.dump('handler_test2.pkl')
+    print datarequest.datacube['tave_global']
+    print datarequest.datacube['lswi_large']
 
 
 if __name__ == "__main__":
     example1()
-
-
+    example2()
