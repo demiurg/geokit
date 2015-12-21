7aed565e5e9ee1650bb148a061ee74a4dfcea6ac
Brandan Glendenning <notbrandan@gmail.com>
2015-12-21 18:25:54 -0500

    sieve metadata fields

--- sieve/static/js/sieve.js
+++ sieve/static/js/sieve.js
@@ -2,12 +2,29 @@ var Sieve = React.createClass({
   displayName: 'Sieve',
   render: function() {
     return (
-      <div>asdf {this.props.property}</div>
+      <div className="sieve">
+        <MetaData title={this.props.title} description={this.props.description} />
+      </div>
+    );
+  }
+});
+
+var MetaData = React.createClass({
+  render: function() {
+    return (
+      <div className="sieve-metadata">
+        <div className="sieve-metadata-title">
+          <input type="text" placeholder="Title..." value={this.props.title} />
+        </div>
+        <div className="sieve-metadata-description">
+          <textarea placeholder="Description...">{this.props.description}</textarea>
+        </div>
+      </div>
     );
   }
 });
 
 ReactDOM.render(
-  <Sieve property="property" />,
+  <Sieve title={data.title} description={data.description} />,
   document.getElementById("sieve-container")
 );
