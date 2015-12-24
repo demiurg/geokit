d0f1424953aec058698c2cc7705ab6fec597e0c4
Brandan Glendenning <notbrandan@gmail.com>
2015-12-24 04:46:51 -0500

    static aggregate toolbar, panelized components for visual separation

--- sieve/static/js/sieve.js
+++ sieve/static/js/sieve.js
@@ -1,20 +1,39 @@
 var Table = ReactBootstrap.Table;
+var Panel = ReactBootstrap.Panel;
+var ButtonGroup = ReactBootstrap.ButtonGroup;
+var ButtonToolbar = ReactBootstrap.ButtonToolbar;
+var Button = ReactBootstrap.Button;
 
 class Sieve extends React.Component {
   render() {
     return (
       <div className="sieve">
-        <MetaData title={this.props.title} description={this.props.description} />
-        <div className="sieve-table-input">
-          a table of input data is here
-          <SieveTable cols={this.props.data.cols} rows={this.props.data.rows} />
-        </div>
-        <SpatialConfigurator />
-        <TemporalConfigurator />
-        <div className="sieve-table-output">
-          a table of output data is here
-          <SieveTable cols={this.props.data.cols} rows={this.props.data.rows} />
-        </div>
+        <Panel>
+          <MetaData title={this.props.title} description={this.props.description} />
+        </Panel>
+        <Panel>
+          <div className="sieve-table-input">
+            a table of input data is here
+            <SieveTable cols={this.props.data.cols} rows={this.props.data.rows} />
+          </div>
+        </Panel>
+        <Panel>
+          <SpatialConfigurator />
+        </Panel>
+        <Panel>
+          <TemporalConfigurator />
+        </Panel>
+        <Panel>
+          <div className="sieve-aggregate">
+            <Aggregate />
+          </div>
+        </Panel>
+        <Panel>
+          <div className="sieve-table-output">
+            a table of output data is here
+            <SieveTable cols={this.props.data.cols} rows={this.props.data.rows} />
+          </div>
+        </Panel>
       </div>
     );
   }
@@ -88,6 +107,34 @@ class TableBody extends React.Component {
       return <tr>{data}</tr>
     });
     return <tbody>{rows}</tbody>;
+  }
+}
+
+class Aggregate extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {aggregate:null};
+  }
+  aggregateMethodToggle(method) {
+    this.setState({aggregate: method});
+  }
+  render() {
+    return (
+      <ButtonToolbar>
+        <ButtonGroup>
+          <Button onClick={this.aggregateMethodToggle.bind(this, "space")} active={this.state.aggregate === "space" ? true : null}>Aggregate Across Space</Button>
+          <Button onClick={this.aggregateMethodToggle.bind(this, "time")} active={this.state.aggregate === "time" ? true : null}>Aggregate Across Time</Button>
+          <Button onClick={this.aggregateMethodToggle.bind(this, null)} active={this.state.aggregate === null ? true : null}>Do Not Aggregate</Button>
+        </ButtonGroup>
+        <ButtonGroup>
+          <Button>Mean</Button>
+          <Button>Median</Button>
+          <Button>Mode</Button>
+          <Button>Range</Button>
+          <Button>Standard Deviation</Button>
+        </ButtonGroup>
+      </ButtonToolbar>
+    );
   }
 }
 
