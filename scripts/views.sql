0d876b0886b78c94d128d8c3fe7a32fe72821ade
Pavel <pavel@ags.io>
2016-03-03 13:07:04 -0500

    Allow publishing for site owners

--- scripts/views.sql
+++ scripts/views.sql
@@ -1,7 +1,17 @@
+
+Definition
  SELECT 1 AS id,
     ( SELECT auth_group.id
            FROM auth_group
           WHERE auth_group.name::text = 'Editors'::text) AS group_id,
+    ( SELECT account_geokitsite.user_id
+           FROM account_geokitsite
+          WHERE account_geokitsite.schema_name::name = "current_schema"()) AS user_id
+UNION
+ SELECT 2 AS id,
+    ( SELECT auth_group.id
+           FROM auth_group
+          WHERE auth_group.name::text = 'Moderators'::text) AS group_id,
     ( SELECT account_geokitsite.user_id
            FROM account_geokitsite
           WHERE account_geokitsite.schema_name::name = "current_schema"()) AS user_id;
