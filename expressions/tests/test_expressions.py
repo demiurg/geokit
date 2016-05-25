2c3f5b8d0967547e31d06636e1b2f8c8e7901ab9
Tom Olson <tolson@appliedgeosolutions.com>
2016-05-25 15:39:49 -0400

    Fix & refactor tests for Expression objects:
    Fix temporal test by mocking around a missing feature.  Fix spatial test by
    fixing 'foo__bar' syntax.  Add test to check aggregation over null data set.
    Refactor for line length and unnecessary names.

--- expressions/tests/test_expressions.py
+++ expressions/tests/test_expressions.py
@@ -3,13 +3,14 @@ from datetime import date
 from django.contrib.auth.models import User
 from django.test import TestCase
 
-from numpy import mean
+import numpy as np
 import sympy
 import factory
 import mock
 from psycopg2.extras import DateRange
 
 from expressions.models import Expression, FormVariable
+from expressions.helpers import ExpressionResult
 from layers.models import Layer, Feature
 
 
@@ -38,7 +39,8 @@ class OneDimensionalExpressionTests(TestCase):
         exp = Expression(name='exp', expression_text='1+2')
         self.assertEqual(exp.evaluate(UserFactory.create()).unpack(), 3)
 
-    @mock.patch.object(Expression, 'resolve_sub_expression', patched_resolve_sub_expression)
+    @mock.patch.object(Expression, 'resolve_sub_expression',
+            patched_resolve_sub_expression)
     def test_sub_expressions(self):
         exp = Expression(name='exp_with_subs', expression_text='expression__exp1 + 1')
         self.assertEqual(exp.evaluate(UserFactory.create()).unpack(), 4)
@@ -50,46 +52,61 @@ class OneDimensionalExpressionTests(TestCase):
         self.assertEqual(exp.evaluate(UserFactory.create(username='tester2')).unpack(), 5)
 
 
-layer = Layer()
-two_dim_spatial_dom_features = [
-    Feature(layer=layer, geometry=None, properties={'var1': 1}),
-    Feature(layer=layer, geometry=None, properties={'var1': 2}),
-    Feature(layer=layer, geometry=None, properties={'var1': 3}),
-    Feature(layer=layer, geometry=None, properties={'var1': 4}),
-    Feature(layer=layer, geometry=None, properties={'var1': 5}),
-]
-two_dim_temporal_dom_features = [
-    Feature(layer=layer, geometry=None, properties={'var1_20161': 1, 'var1_20162': 2, 'var1_2016_3': 3})
-]
-
 def return_two_dim_spatial_dom_features(*args, **kwargs):
-    return two_dim_spatial_dom_features
+    layer = Layer()
+    return [
+        Feature(layer=layer, geometry=None, properties={'var1': 1}),
+        Feature(layer=layer, geometry=None, properties={'var1': 2}),
+        Feature(layer=layer, geometry=None, properties={'var1': 3}),
+        Feature(layer=layer, geometry=None, properties={'var1': 4}),
+        Feature(layer=layer, geometry=None, properties={'var1': 5}),
+    ]
 
 
 class TwoDimensionalExpressionTest(TestCase):
-    @mock.patch('layers.models.Feature.objects.filter', return_two_dim_spatial_dom_features)
-    def test_over_space(self):
+    @mock.patch('expressions.models.sympy.sympify')
+    def test_mean_across_nothing(self, sympify_mock):
+        """Confirm that the mean of an empty dataset is NaN."""
+        er = ExpressionResult()
+        sympify_mock.return_value = er
+
         exp = Expression(
             name='exp_over_space',
-            expression_text='layer_var1',
-            aggregate_dimension='SP',
-            aggregate_method='MEA'
-        )
-        self.assertEqual(
-            exp.evaluate(UserFactory.create()).vals,
-            sympy.Matrix([[1], [2], [3], [4], [5]])
-        )
-
-    @mock.patch.object(Expression, 'spatial_domain_features', two_dim_temporal_dom_features)
-    def test_over_time(self):
-        exp = Expression(
-            name='exp_over_time',
-            expression_text='layer_var1',
-            temporal_domain=DateRange(date(2016, 1, 2), date(2016, 1, 3)),
+            expression_text='layer__var1', # ignored due to mocking
             aggregate_dimension='TM',
             aggregate_method='MEA'
         )
-        self.assertEqual(
-            exp.evaluate(UserFactory.create()),
-            mean([2, 3])
+        actual = exp.evaluate(UserFactory.create()).unpack()
+        self.assertTrue(np.isnan(actual))
+
+    @mock.patch('layers.models.Feature.objects.filter',
+            return_two_dim_spatial_dom_features)
+    def test_mean_across_space(self):
+        """Take the mean of a value aggregated across several Features."""
+        exp = Expression(
+            name='exp_over_space',
+            expression_text='layer__var1',
+            aggregate_dimension='SP',
+            aggregate_method='MEA'
         )
+        actual = exp.evaluate(UserFactory.create()).unpack()
+        self.assertEqual(3.0, actual)
+
+    @mock.patch('expressions.models.sympy.sympify')
+    def test_mean_across_time(self, sympify_mock):
+        """Verify computation of a mean across time-series data.
+
+        For now this is just mocking an ExpressionResult since Expression has
+        no way to directly load geokit_table.Record objects.
+        """
+        er = ExpressionResult([[1, 2, 3], [2, 4, 6], [5, 10, 15]])
+        sympify_mock.return_value = er
+        exp = Expression(
+            name='exp_over_time',
+            expression_text='layer__var1', # ignored due to mocking
+            aggregate_dimension='TM',
+            aggregate_method='MEA'
+        )
+        expected = ExpressionResult([[2], [4], [10]])
+        actual = exp.evaluate(UserFactory.create())
+        self.assertEqual(expected, actual)
