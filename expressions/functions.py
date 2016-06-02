5f57377541f2b7be3c54d90a5d5d3ab1cbdbd73b
Nate Rubin <nrubin@wildcats.unh.edu>
2016-06-02 13:03:55 -0400

    Refactor join operation out of Join() function

--- expressions/functions.py
+++ expressions/functions.py
@@ -1,11 +1,11 @@
 from django.apps import apps
-from django.db import connection
 
 import numpy as np
 import sympy
-from sortedcontainers import SortedDict
 
 from expressions.helpers import ExpressionResult
+
+from .helpers import join_layer_and_table
 
 
 class Join(sympy.Function):
@@ -21,36 +21,7 @@ class Join(sympy.Function):
         layer_type, layer_name, layer_field = str(layer).split('__')
         table_type, table_name, table_field = str(table).split('__')
 
-        query = """
-            SELECT t1.id, t1.properties, t2.id, t2.properties, t2.date FROM """ +\
-            connection.schema_name + """.layers_feature t1
-                JOIN """ + connection.schema_name + """.geokit_tables_record t2
-                ON t1.properties->>%s = t2.properties->>%s
-                WHERE t1.layer_id = %s AND t2.table_id = %s
-        """
-        cursor = connection.cursor()
-        cursor.execute(query, [layer_field, table_field, layer_name, table_name])
-
-        # dict of dicts:  outer key is feature IDs, inner key is dates, inner
-        # values are geokit_tables.Record.properties.
-        results = {}
-        for (f_id, _, _, t_props, t_date) in cursor.fetchall():
-            if f_id not in results:
-                results[f_id] = SortedDict()
-            results[f_id][t_date] = t_props
-
-        if results == {}: # did you find any data?
-            return ExpressionResult()
-
-        spatial_key = results.keys() # list of layers_feature.id
-        # list of geokit_tables_record.date
-        temporal_key = list(results[spatial_key[0]].keys())
-
-        # result_matrix is just the natural list of lists taken from results,
-        # but with the keys removed.
-        result_matrix = []
-        for _, vals in results.iteritems():
-            result_matrix.append(vals.values())
+        result_matrix, temporal_key, spatial_key = join_layer_and_table(layer_name, layer_field, table_name, table_field)
 
         return ExpressionResult(result_matrix, temporal_key, spatial_key)
 
