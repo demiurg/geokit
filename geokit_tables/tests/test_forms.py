ff821e62fa6cf0c30a7dfc4db3f9a987ba28fdbd
Tom Olson <tolson@appliedgeosolutions.com>
2016-06-08 14:33:37 -0400

    Refactor tests to use common fixture

--- geokit_tables/tests/test_forms.py
+++ geokit_tables/tests/test_forms.py
@@ -196,23 +196,23 @@ test_data = u"""fid,date,srad,prcp,tmin,tmax
 0,2010-183,27.474,0.0,13.0,27.0
 """
 
-@pytest.mark.django_db
-def test_gktf_normal_case(set_tenant):
-    """Check validation success with normal inputs."""
+@pytest.fixture
+def filled_gktf():
+    """Set up a GeoKitTableForm with CSV for validation."""
     with io.StringIO(test_data) as fake_csv:
         post_vars = {'date_column': 'date',
                      'name': 'testtable', 'csv_file': fake_csv}
         req = RequestFactory().post('/shouldnt/matter', post_vars)
         gktf = GeoKitTableForm(req.POST, req.FILES)
-        assert gktf.is_valid()
+    return gktf
 
 @pytest.mark.django_db
-def test_gktf_date_field_mismatch(set_tenant):
-    """Confirm ValidationError emitted when date field names don't match."""
-    with io.StringIO(test_data) as fake_csv:
-        post_vars = {'date_column': 'I-M-RONG-FLD-NAME-LOL',
-                     'name': 'testtable', 'csv_file': fake_csv}
-        req = RequestFactory().post('/shouldnt/matter', post_vars)
-        gktf = GeoKitTableForm(req.POST, req.FILES)
-        assert not gktf.is_valid()
+def test_gktf_normal_case(set_tenant, filled_gktf):
+    """Check validation success with normal inputs."""
+    assert filled_gktf.is_valid()
 
+@pytest.mark.django_db
+def test_gktf_date_field_mismatch(set_tenant, filled_gktf):
+    """Form shouldn't be valid when date field name isn't in CSV."""
+    filled_gktf.data['date_column'] = 'I-M-RONG-FLD-NAME-LOL'
+    assert not filled_gktf.is_valid()
