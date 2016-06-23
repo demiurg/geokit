03ff14a6c73ea7159004624aa75cce4f5bea9090
Nate Rubin <nrubin@wildcats.unh.edu>
2016-06-23 16:05:47 -0400

    Convert graph, map, table template tags to use variables

---
+++ variables/blocks.py
@@ -0,0 +1,17 @@
+from django.utils.functional import cached_property
+
+from wagtail.wagtailcore.blocks import ChooserBlock
+
+
+class VariableChooserBlock(ChooserBlock):
+    @cached_property
+    def target_model(self):
+        from variables.models import Variable
+        return Variable
+
+    @cached_property
+    def widget(self):
+        pass
+
+    def get_prep_value(self, variable):
+        return variable.id
