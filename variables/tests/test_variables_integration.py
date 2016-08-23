8c231414934d146e9f5f5ce4915e5bcf7edfa329
Pavel <pavel@ags.io>
2016-08-23 18:11:16 -0400

    Two tests pass

--- variables/tests/test_variables_integration.py
+++ variables/tests/test_variables_integration.py
@@ -42,3 +42,48 @@ def test_example_0_5(set_schema):
     t_norm_summer = Variable(tree=['tmean', [t_summer.data()]])
 
     np.testing.assert_array_equal(t_norm_summer.data()['values'], np.array([[13.175], [22.4]]))
+
+
+@pytest.mark.django_db
+def test_example_0_5_select(set_schema):
+    tmin = Variable(tree=[
+        'select', [
+            ['join', [
+                {'model': 'Layer', 'id': 'cnty24k97', 'field': 'fid'},
+                {'model': 'Table', 'id': 'cnty24k97_data', 'field': 'fid'},
+            ]],
+            'tmin'
+        ]
+    ])
+    tmax = Variable(tree=[
+        'select', [
+            ['join', [
+                {'model': 'Layer', 'id': 'cnty24k97', 'field': 'fid'},
+                {'model': 'Table', 'id': 'cnty24k97_data', 'field': 'fid'},
+            ]],
+            'tmax'
+        ]
+    ])
+    t_summer = Variable(
+        tree=['tfilter', [
+            ['mean', [
+                tmin.data(),
+                tmax.data()
+            ]],
+            {
+                'filter_type': 'inclusive',
+                'date_ranges': [
+                    {'start': date(2010, 6, 1), 'end': date(2010, 8, 31)},
+                    {'start': date(2011, 6, 1), 'end': date(2011, 8, 31)},
+                    {'start': date(2012, 6, 1), 'end': date(2012, 8, 31)},
+                    {'start': date(2013, 6, 1), 'end': date(2013, 8, 31)},
+                    {'start': date(2014, 6, 1), 'end': date(2014, 8, 31)}
+                ]
+            }
+        ]],
+        #spatial_domain=list(Feature.objects.filter(layer='cnty24k97')),
+        #temporal_domain=rrule(DAILY, dtstart=date(2010, 1, 1), until=date(2014, 12, 31))
+    )
+    t_norm_summer = Variable(tree=['tmean', [t_summer.data()]])
+
+    np.testing.assert_array_equal(t_norm_summer.data()['values'], np.array([[13.175], [22.4]]))
