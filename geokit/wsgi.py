046c55a0cf0cf82f251809e8bc7f48a15c30baac
Pavel <pavel@ags.io>
2017-02-21 13:32:55 -0500

    This should not have been commented out

--- geokit/wsgi.py
+++ geokit/wsgi.py
@@ -5,8 +5,8 @@ _cwd = os.path.realpath(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, os.path.dirname(_cwd))
 sys.path.insert(0, _cwd)
 
-#activate_this = os.path.realpath(os.path.join(_cwd, './../.venv/bin/activate_this.py'))
-#execfile(activate_this, dict(__file__=activate_this))
+activate_this = os.path.realpath(os.path.join(_cwd, './../.venv/bin/activate_this.py'))
+execfile(activate_this, dict(__file__=activate_this))
 
 from django.core.wsgi import get_wsgi_application
 
