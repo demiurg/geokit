6d08807468f26dc28ca778fe591a635504e84d18
Brandan Glendenning <notbrandan@gmail.com>
2017-05-17 00:45:53 -0400

    initial site guide rough draft

--- variables/wagtail_hooks.py
+++ variables/wagtail_hooks.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url, include
+from django.shortcuts import render
 from django.core.urlresolvers import reverse
 
 from wagtail.wagtailcore import hooks
@@ -6,14 +7,22 @@ from wagtail.wagtailadmin.menu import MenuItem
 
 from variables import admin_urls
 
+def site_guide(request):
+    return render(request, 'variables/site_guide.html')
+
 
 @hooks.register('register_admin_urls')
 def register_admin_urls():
     return [
         url('^variables/', include(admin_urls, namespace='variables', app_name='variables')),
+        url('^site-guide/', site_guide, name='site_guide'),
     ]
 
 
 @hooks.register('register_admin_menu_item')
 def register_variables_menu_item():
     return MenuItem('Variables', reverse('variables:index'), classnames="icon icon-placeholder")
+
+@hooks.register('register_admin_menu_item')
+def register_user_guide_menu_item():
+    return MenuItem('Site Guide', reverse('site_guide'), classnames="icon icon-placeholder")
