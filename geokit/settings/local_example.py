56c5ca2f44630d63b8a4cf1575c67367613b71c4
Rick Emery <remery@appliedgeosolutions.com>
2017-05-18 16:29:21 -0400

    adding ALLOWED_HOSTS to local example

--- geokit/settings/local_example.py
+++ geokit/settings/local_example.py
@@ -1,7 +1,8 @@
-from .base import DATABASES, GEOKIT_HOSTS
+from .base import DATABASES, GEOKIT_HOSTS, ALLOWED_HOSTS
 
 ## Add your domain to this list
 #GEOKIT_HOSTS.append('testserver') # django's hostname in testing mode
+#ALLOWED_HOSTS.append('testserver')
 
 #DATABASES['default']['TEST'] = {'NAME': 'test_pavel_geokit_2' }
 
